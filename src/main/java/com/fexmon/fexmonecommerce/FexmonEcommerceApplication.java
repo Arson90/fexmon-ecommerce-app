dc701af08e46948e5c600deeeb0c01b7cb233874
Arson <arek1990cda@gmail.com>
2022-08-23 18:08:25 +0200

    initialization project

---
+++ src/main/java/com/fexmon/fexmonecommerce/FexmonEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.fexmon.fexmonecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FexmonEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FexmonEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fexmon/fexmonecommerce/FexmonEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fexmon.fexmonecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FexmonEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
