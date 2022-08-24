eb2205b7d4619ce5dca2ce983e016a75fb5e264b
Arson <arek1990cda@gmail.com>
2022-08-24 19:08:32 +0200

    Task-3 Create product model

--- src/main/java/com/fexmon/fexmonecommerce/model/UserModel.java
+++ src/main/java/com/fexmon/fexmonecommerce/model/UserModel.java
@@ -9,13 +9,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @RequiredArgsConstructor
-public class UserModel {
+public class UserModel implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
