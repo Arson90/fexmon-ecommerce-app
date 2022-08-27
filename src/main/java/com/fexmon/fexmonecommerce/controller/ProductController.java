2e0aff06ad16ce88433f1b0f6f37e73d03b51046
Arson <arek1990cda@gmail.com>
2022-08-27 09:01:00 +0200

    Task-9 create product controller

--- src/main/java/com/fexmon/fexmonecommerce/controller/ProductController.java
+++ src/main/java/com/fexmon/fexmonecommerce/controller/ProductController.java
@@ -1,17 +1,38 @@
 package com.fexmon.fexmonecommerce.controller;
 
 import com.fexmon.fexmonecommerce.model.ProductModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fexmon.fexmonecommerce.model.UserModel;
+import org.springframework.http.RequestEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("product")
+@RequestMapping("")
 public class ProductController {
 
-    @GetMapping()
-    public ProductModel getProductById() {
+    @GetMapping("/product/{id}")
+    public ProductModel getProductById(@PathVariable final long id) {
         throw new UnsupportedOperationException("Not implemented yet");
+    }
 
+    @GetMapping("/product")
+    public List<ProductModel> getAllProducts() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @PostMapping("/product")
+    public void addProduct(@RequestBody final ProductModel product) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @PutMapping("/product/{id}")
+    public void updateProduct(@RequestBody final ProductModel product, @PathVariable final long id) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @DeleteMapping("/product/{id}")
+    public void deleteProduct(@PathVariable final long id) {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
